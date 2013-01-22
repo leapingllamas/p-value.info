2ab502a3bd78c8e97a616fff66065d98860b4c59
leapingllamas <canderson@onekingslane.com>
2013-01-21 20:52:15 -0800

    initial commit

---
+++ jokes_2013_01/extract_homynyms.py
@@ -0,0 +1,40 @@
+from bs4 import BeautifulSoup
+import urllib2
+from bs4 import Comment
+
+filename="homonyms.html"
+page = open(filename,"r").read()
+# for speed of dev, I just downloaded the file and worked off that.
+# Alterntively, you could do:
+# url="http://www.cooper.com/alan/homonym_list.html"
+# request = urllib2.Request(url)
+# page = urllib2.urlopen(request)
+
+import json
+import urllib2
+def get_part_of_speech(word):
+    '''
+     is this a noun, adjective etc. This API is unofficial but works.
+    '''
+    url="http://www.google.com/dictionary/json?callback=get_definition&q=" + word + "&sl=en&tl=en&restrict=pr%2Cde&client=te"
+    s=urllib2.urlopen(url).read().replace("get_definition(","").replace(",200,null)","")
+    try:
+       return eval(s)['primaries'][0]['terms'][0]['labels'][0]['text']
+    except: 
+       return "unknown"
+
+soup = BeautifulSoup(page)
+results=[]
+comments = soup.findAll(text=lambda text:isinstance(text, Comment))
+for comment in comments:
+    #for now, ignore the 3rd row of the 3 row homonyms
+    try:
+        word1=comment.next_sibling.next_sibling.find_all('td')[1].string.strip()
+        def1=comment.next_sibling.next_sibling.find_all('td')[2].string.strip()
+        pos1=get_part_of_speech(word1).lower()
+        word2=comment.next_sibling.next_sibling.next_sibling.next_sibling.find_all('td')[1].string.strip()
+        def2=comment.next_sibling.next_sibling.next_sibling.next_sibling.find_all('td')[2].string.strip()
+        pos2=get_part_of_speech(word2).lower()
+        print "\t".join([word1,def1,pos1,word2,def2,pos2])
+    except:
+       pass
---
+++ jokes_2013_01/generate_jokes.py
@@ -0,0 +1,28 @@
+def indefinite_article(w):
+    if w.lower().startswith("a ") or w.lower().startswith("an "): return ""
+    return "an " if w.lower()[0] in list('aeiou') else "a "
+
+def joke_type1(d1,d2,w1,w2):
+    return "What do you call " + indefinite_article(d1) + d1 + " " + d2 + "? " + \
+           indefinite_article(w1).upper() + w1 + " " + w2 + "."
+
+def joke_type2(d1,d2,w1,w2):
+    return "When is " + indefinite_article(d1)  + d1 + " like " + indefinite_article(d2) + d2 + "? " + \
+           "When it is " + indefinite_article(w2)  + w2 + "."
+
+data = open("processed_homonyms.txt","r").readlines()
+
+for line in data:
+     [w1,d1,pos1,w2,d2,pos2]=line.strip().split("\t")
+#     if "flat pad" in  line:
+#        print "w1=["+w1+"]"
+#        print "d1=["+d1+"]"
+#        print "w2=["+w2+"]"
+#        print "d2=["+d2+"]"
+     if pos1=='adjective' and pos2=='noun': 
+         print joke_type1(d1,d2,w1,w2)
+     elif pos1=='noun' and pos2=='adjective': 
+         print joke_type1(d2,d1,w2,w1)
+     elif pos1=='noun' and pos2=='noun': 
+         print joke_type2(d1,d2,w1,w2)
+         print joke_type2(d2,d1,w2,w1)
